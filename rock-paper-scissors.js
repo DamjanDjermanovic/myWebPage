baa33e844a8a4becdf1d3942284029fc8297ecba
Damjan Djermanovic <djermanovicdamjan@gmail.com>
2023-06-28 09:19:27 +0200

    Add rock-paper-scissors functions.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,23 +5,62 @@ let computerScore = 0;
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+const playerScoreBoard = document.querySelector('#playerScoreBoard');
+const computerScoreBoard = document.querySelector('#computerScoreBoard');
+const cpuChoice = document.querySelector('#cpuChoice');
 
 function getComputerChoice() {
     return CHOICES[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        return 'tie';
-      } else if (
-        (playerChoice === 'rock' && computerChoice === 'scissors') ||
-        (playerChoice === 'paper' && computerChoice === 'rock') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper')
-      ) {
-        return 'player';
-      } else {
-        return 'computer';
-      }
+  if (playerChoice === computerChoice) {
+      return 'tie';
+    } else if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      return 'player';
+    } else {
+      return 'computer';
+    }
 }
 
-rock.addEventListener('click', function
+function logScore(result) {
+  if (result === "player") {
+    playerScore ++;
+    playerScoreBoard.textContent = "Player: " + playerScore;
+  }
+  if (result === "computer") {
+    computerScore ++;
+    computerScoreBoard.textContent = "Computer: " + computerScore;
+  }
+}
+
+rock.addEventListener('click', () => {
+  let computerChoice = getComputerChoice();
+  let playerChoice = CHOICES[0];
+
+  let result = playRound(playerChoice, computerChoice);
+
+  logScore(result);
+})
+
+paper.addEventListener('click', () => {
+  let computerChoice = getComputerChoice();
+  let playerChoice = CHOICES[1];
+
+  let result = playRound(playerChoice, computerChoice);
+
+  logScore(result);
+})
+
+scissors.addEventListener('click', () => {
+  let computerChoice = getComputerChoice();
+  let playerChoice = CHOICES[2];
+
+  let result = playRound(playerChoice, computerChoice);
+
+  logScore(result);
+})
